23a2876bb0684f9a7726a9a8452c9eef529e1b8d
Konjac-XZ <1951801592@qq.com>
2026-02-15 19:14:44 +0800

    init: first commit

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+$profileRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+$profileScripts = Join-Path $profileRoot "Scripts\Profile"
+
+$startupScripts = @(
+  "10-modules.ps1"
+  "20-prompt.ps1"
+  "30-theme.ps1"
+  "40-aliases.ps1"
+  "80-psreadline.ps1"
+  "81-fzf.ps1"
+  "82-zoxide.ps1"
+)
+
+foreach ($script in $startupScripts) {
+  $scriptPath = Join-Path $profileScripts $script
+  if (Test-Path $scriptPath) {
+    . $scriptPath
+  }
+}
