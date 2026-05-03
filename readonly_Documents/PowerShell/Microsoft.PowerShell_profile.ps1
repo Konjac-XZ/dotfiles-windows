100f35c7ae58c6aba6b6d9ba5b45fd626a8871a9
Konjac-XZ <1951801592@qq.com>
2026-05-03 14:36:45 +0800

    chore: sync

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,32 @@
 $profileRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
 $profileScripts = Join-Path $profileRoot "Scripts\Profile"
 
+function Test-InteractiveSession {
+  if (-not [Environment]::UserInteractive) {
+    return $false
+  }
+
+  try {
+    if ([Console]::IsInputRedirected -or [Console]::IsOutputRedirected) {
+      return $false
+    }
+  } catch {
+    return $false
+  }
+
+  return $null -ne $Host.UI -and $null -ne $Host.UI.RawUI
+}
+
+if (-not (Test-InteractiveSession)) {
+  return
+}
+
 $startupScripts = @(
   "10-modules.ps1"
   "20-prompt.ps1"
   "30-theme.ps1"
   "40-aliases.ps1"
+  "50-env.ps1"
   "80-psreadline.ps1"
   "81-fzf.ps1"
   "82-zoxide.ps1"
